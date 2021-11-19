0071beaa4a9c37e38f0458bfb202fda0d707d5e4
Anik-Dey.exe <67157750+AnikDey-exe@users.noreply.github.com>
2021-11-19 14:33:42 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import DetailsScreen from './screens/DetailsScreen';
+import HomeScreen from './screens/HomeScreen';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+export default function App() {
+  return (
+    <AppContainer/>
+  );
+}
+
+const appStackNavigator = createStackNavigator(
+  {
+    Home: { screen: HomeScreen, navigationOptions: { headerShown: false } },
+    Details: { screen: DetailsScreen }
+  },
+  {
+    initialRouteName: 'Home'
+  }
+)
+
+const AppContainer = createAppContainer(appStackNavigator)
